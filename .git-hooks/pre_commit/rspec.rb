9fe01cfe60ce509e0163288f3875816e37cbcae1
Daniel Silvestre <djlebersilvestre@gmail.com>
2015-03-09 16:17:03 -0300

    Rspec plugin for overcommit - avoiding broken commits

---
+++ .git-hooks/pre_commit/rspec.rb
@@ -0,0 +1,21 @@
+module Overcommit
+  module Hook
+    module PreCommit
+      class Rspec < Base
+        def run
+          success = system("#{rspec_command}")
+
+          return :fail, 'Error running specs' unless success
+
+          :pass
+        end
+
+        private
+
+        def rspec_command
+          'bundle exec rspec'
+        end
+      end
+    end
+  end
+end

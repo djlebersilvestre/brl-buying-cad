28c69be318fb5191f68e4beef01d6f66e64dfd3d
Daniel Silvestre <djlebersilvestre@gmail.com>
2015-03-10 18:23:01 -0300

    Not interested in post-checkout hooks

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -e
+
 sudo apt-get install shellcheck
 
 bundle install --path vendor/bundle
 
 gem install rubocop
 gem install overcommit
+
+mv .git/hooks/post-checkout .git/hooks/post-checkout.sample

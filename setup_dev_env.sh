0c6a3bd33ee188ea9d0cbb6844b80999621219e3
Daniel Silvestre <djlebersilvestre@gmail.com>
2015-03-10 20:26:27 -0300

    Better setup script ;)

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -3,7 +3,12 @@
 set -e
 cd "$(dirname "$0")"
 
-sudo echo
+# Dependency for overcommit
+sudo apt-get install shellcheck
+
+echo 'Using RVM (loading from .bash_profile)'
+source $HOME/.bash_profile
+rvm use 2.2.1@brl-buying-cad --create
 
 echo 'Vendorizing gems'
 bundle install --path vendor/bundle
@@ -12,11 +17,10 @@ echo 'Adding base gems to gemset'
 gem install rubocop
 gem install overcommit
 
-echo 'Configuring overcommit'
+echo 'Configuring overcommit (to apply git hooks - quality threshold)'
 overcommit --install
 overcommit --sign pre-commit
 mv .git/hooks/post-checkout .git/hooks/post-checkout.sample
-sudo apt-get install shellcheck
 
-echo 'Running specs for the first time'
+echo 'Running specs for the first time  ;)'
 bundle exec spring rspec

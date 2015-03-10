dbbfd061bd105575abe632c2f5b33f16ee5dc347
Daniel Silvestre <djlebersilvestre@gmail.com>
2015-03-10 19:48:43 -0300

    Better setup script ;)

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 set -e
+cd "$(dirname "$0")"
 
-# Vendorizing gems
+sudo echo
+
+echo 'Vendorizing gems'
 bundle install --path vendor/bundle
 
-# Adding base gems to gemset
+echo 'Adding base gems to gemset'
 gem install rubocop
 gem install overcommit
 
-# Configuring overcommit
+echo 'Configuring overcommit'
 overcommit --install
 overcommit --sign pre-commit
 mv .git/hooks/post-checkout .git/hooks/post-checkout.sample
 sudo apt-get install shellcheck
 
+echo 'Running specs for the first time'
 bundle exec spring rspec

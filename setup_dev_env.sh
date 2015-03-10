0a63da73e34abd328c3e673f6038f49dc4bcb758
Daniel Silvestre <djlebersilvestre@gmail.com>
2015-03-10 18:44:53 -0300

    Testing and improving initial setup script

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -2,11 +2,15 @@
 
 set -e
 
-sudo apt-get install shellcheck
-
+# Vendorizing gems
 bundle install --path vendor/bundle
 
+# Adding base gems to gemset
 gem install rubocop
 gem install overcommit
 
+# Configuring overcommit
+overcommit --install
+overcommit --sign pre-commit
 mv .git/hooks/post-checkout .git/hooks/post-checkout.sample
+sudo apt-get install shellcheck

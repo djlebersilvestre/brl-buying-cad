9e2cf9df5d8862d24da6a04761ff67afdd2a3487
Daniel Silvestre <djlebersilvestre@gmail.com>
2015-03-12 16:05:31 -0300

    Testing last_run commits

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,8 @@ require 'simplecov'
 
 SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 SimpleCov.start do
-  `git checkout -q coverage/.last_run.json`
+  `git fetch`
+  `git checkout origin/master coverage/.last_run.json`
 
   minimum_coverage(90)
   maximum_coverage_drop(1)

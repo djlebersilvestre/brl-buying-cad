32f9f056b11c46e23bdb47ea0758a7e273a576d4
Daniel Silvestre <djlebersilvestre@gmail.com>
2015-03-12 16:07:19 -0300

    Coverage drop authorized till 2%

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ SimpleCov.start do
   `git checkout origin/master coverage/.last_run.json`
 
   minimum_coverage(90)
-  maximum_coverage_drop(1)
+  maximum_coverage_drop(2)
 
   add_filter 'spec/'
   add_filter 'config/'

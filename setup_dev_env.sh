c6ed50805ce84154d6c0ac8753f68d7c063bb1d1
Daniel Silvestre <djlebersilvestre@gmail.com>
2015-03-17 12:40:15 -0300

    Now we have docker in the setup - under tests

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -3,13 +3,11 @@
 set -e
 cd "$(dirname "$0")"
 
-# Dependency for overcommit and postgres
-sudo apt-get install -y shellcheck libpq-dev
+echo 'Installing PostgreSQL lib - for the driver'
+sudo apt-get install -y libpq-dev shellcheck
 
-# TODO: we must prepare docker environment
-
-echo 'Using RVM (loading from .bash_profile)'
-source "$HOME"/.bash_profile
+echo 'Loading RVM - it must be installed as user mode'
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 rvm use 2.2.1@brl-buying-cad --create
 
 echo 'Vendorizing gems'
@@ -17,6 +15,7 @@ bundle install --path vendor/bundle
 
 echo 'Adding base gems to gemset'
 gem install rubocop
+gem install rubycritic
 gem install overcommit
 
 echo 'Configuring overcommit (to apply git hooks - quality threshold)'
@@ -24,5 +23,22 @@ overcommit --install
 overcommit --sign pre-commit
 mv .git/hooks/post-checkout .git/hooks/post-checkout.sample
 
-echo 'Running specs for the first time  ;)'
+echo 'Setting up docker environment - dependencies of the application'
+# TODO: create linux-scripts to auto install and configure docker
+
+echo 'Setting up Postgres over docker'
+source <(curl https://raw.githubusercontent.com/djlebersilvestre/docker-postgres/master/commands.sh)
+pg-setup
+pg-start
+bundle exec rake db:setup
+
+echo 'Setting up Redis over docker'
+source <(curl https://raw.githubusercontent.com/djlebersilvestre/linux-scripts/master/dkr-redis-commands.sh)
+redis-setup
+redis-start
+
+echo 'Finally, testing for the first time'
 bundle exec spring rspec
+
+echo 'Running everything attached to overcommit (specs too)  ;)'
+overcommit -r

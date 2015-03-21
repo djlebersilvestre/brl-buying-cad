bdc9801f4e75be813b3b3e143261e0914074f21b
Daniel Silvestre <djlebersilvestre@gmail.com>
2015-03-21 07:50:27 -0300

    Redis with pass

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,11 +3,13 @@ require 'sidekiq/web'
 # TODO: improve this initializer - use config files
 redis_host = 'redis://127.0.0.1:6379'
 redis_namespace = 'sidekiq:brl_buying_cad'
+redis_password = 'testing'
 
 Sidekiq.configure_server do |config|
   config.redis = {
     url: redis_host,
     namespace: redis_namespace,
+    password: redis_password
   }
   config.failures_default_mode = :all
 end
@@ -16,5 +18,6 @@ Sidekiq.configure_client do |config|
   config.redis = {
     url: redis_host,
     namespace: redis_namespace,
+    password: redis_password
   }
 end

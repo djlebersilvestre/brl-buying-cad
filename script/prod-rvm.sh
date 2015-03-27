68ce6e359db947182eca028163e3740268842873
Daniel Silvestre <djlebersilvestre@gmail.com>
2015-03-27 11:34:19 -0300

    Using default PATH variable

--- script/prod-rvm.sh
+++ script/prod-rvm.sh
@@ -7,5 +7,5 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 curl -sSL https://get.rvm.io | bash -s stable --ruby=ruby-"$RUBY_VERSION"
 
 echo "source $RVM_HOME/scripts/rvm" >> /etc/bash.bashrc
-echo "export PATH=$PATH:$RVM_HOME/bin" >> /etc/profile
+echo "export PATH="'$PATH'":$RVM_HOME/bin" >> /etc/profile
 source /etc/profile

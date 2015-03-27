46f574db57599d94c68f8a4e9830cf5935d4747b
Daniel Silvestre <djlebersilvestre@gmail.com>
2015-03-27 10:12:23 -0300

    Other base packages are needed for a fresh install

--- script/prod-packages.sh
+++ script/prod-packages.sh
@@ -4,4 +4,4 @@ set -e
 
 echo "Installing PostgreSQL lib for the driver and git for deployment"
 apt-get update && apt-get upgrade -y \
-  && apt-get install -y libpq-dev git daemontools
+  && apt-get install -y procps screen curl vim libpq-dev git daemontools

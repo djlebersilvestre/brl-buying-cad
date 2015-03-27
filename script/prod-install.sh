96170d05efd795f20a8ddcd3e8c30dfdf47deb4c
Daniel Silvestre <djlebersilvestre@gmail.com>
2015-03-27 09:56:29 -0300

    Double quote to prevent globbing and word splitting

--- script/prod-install.sh
+++ script/prod-install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 set -e
-source ${0%/*}/prod-vars.sh
+source "${0%/*}"/prod-vars.sh
 
 echo "Configuring app under $APP_DIR"
-mkdir -p $APP_DIR
-git clone https://github.com/djlebersilvestre/brl-buying-cad.git $APP_DIR
-mkdir -p $APP_DIR/tmp/pids
+mkdir -p "$APP_DIR"
+git clone https://github.com/djlebersilvestre/brl-buying-cad.git "$APP_DIR"
+mkdir -p "$APP_DIR"/tmp/pids
 
 echo "Vendorizing gems"
-cd $APP_DIR
+cd "$APP_DIR"
 source /usr/local/rvm/scripts/rvm
 bundle install --path vendor/bundle --without development test
